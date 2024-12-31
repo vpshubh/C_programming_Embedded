16c442416f594f5d784dfb4b09fbbd79483f42fb
vpshubh <chakravartyshubh23@gmail.com>
2024-12-31 22:48:22 +0530

    ok

--- Programmes/Functions_C_Udemy/Malloc_pointer.cpp
+++ Programmes/Functions_C_Udemy/Malloc_pointer.cpp
@@ -18,6 +18,12 @@ int main()
     {
         cout << p[i] << endl; // arrray inside heap
     }
+    // Note: If we are dynamically creating the memory for an array we have to delete that memory using free(),
+
+    // and if we are sing c- language then we have to use free() p
+    // heap memory should be deallocated
+
+    free(p);
     return 0;
 } */
 
@@ -30,6 +36,10 @@ using namespace std;
 int main()
 {
     int *p;
+    // C++ syntax
+    // Using new keyword we have dynamically created the memory for an array, in heap
+    // so if we are creating the memeory like this wee ave to also delete that memory
+    // using delete keyword
     p = new int[5]; // only p is inside of stack and the array of 5 integers is inside heap
     p[0] = 10;
     p[1] = 15;
@@ -40,5 +50,9 @@ int main()
     {
         cout << p[i] << endl; // arrray inside heap
     }
+    delete[] p; // delete the memory  if it is any of the variable then we have to write like, delete "variable name"
+                // but for an array we have to write delete[] in C++
+                // heap memory should be deallocated
+
     return 0;
 }
